edda28b7bf5cecbff8ab28e00428a5df6067e784
kennedymwaniki <kennedymwaniki089@gmail.com>
2024-09-20 07:25:14 +0300

    rate limit of requests made

--- tweetty.py
+++ tweetty.py
@@ -1,8 +1,10 @@
 import tweepy
 
+import time
 
 auth = tweepy.OAuthHandler('oqV9w4QdJ0pxdKUM5ShrJB9tI', 'pijTDklJWBYtYktuLPRBg3pwpXK2tLjyo8hwYblPQGPPpD4g75')
-auth.set_access_token('1834114836231274496-Mhd3vF4bRlXAYnaEFQwcI4CMCHVjNk', 'QeOTnX1A7yEyaRQjTfUswlB0eMmgu9yPgBd2snzfN47UE')
+auth.set_access_token('1834114836231274496-Mhd3vF4bRlXAYnaEFQwcI4CMCHVjNk',
+                      'QeOTnX1A7yEyaRQjTfUswlB0eMmgu9yPgBd2snzfN47UE')
 
 api = tweepy.API(auth)
 
@@ -15,8 +17,20 @@ client = tweepy.Client(consumer_key='oqV9w4QdJ0pxdKUM5ShrJB9tI',
 # Get the authenticated user's information
 user = client.get_me()
 
-print(user.data.name)
 
+# if we get a rate limit error we pause the application
+def handle_limit(cursor):
+    try:
+        while True:
+            yield cursor.next()
+
+    except tweepy.RateLimitError:
+        time.sleep(1000)
+
+
+# bot
+for follower in handle_limit(tweepy.Cursor(api.get_followers()).items()):
+    print(follower.name)
 
 # for this to work you need the basic x plan which is 100$
 # public_tweets = api.home_timeline()
